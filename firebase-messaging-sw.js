672cdd9f833dca9763fa4998c302467a7b59481a
DarwishKhan1 <darwishkhan772@gmail.com>
2021-02-07 02:50:37 -0800

    Updates

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,15 @@
+// importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js");
+
+// firebase.initializeApp({
+//     apiKey: "AIzaSyBtKpn7vEemd3V0SCMJp7v6QcUAR5_Bh3s",
+//     authDomain: "fooddelivery-95b2e.firebaseapp.com",
+//     databaseURL: "https://fooddelivery-95b2e.firebaseio.com",
+//     projectId: "fooddelivery-95b2e",
+//     storageBucket: "fooddelivery-95b2e.appspot.com",
+//     messagingSenderId: "441958838421",
+//     appId: "1:441958838421:web:121226e3b50a11c9772b9c",
+//     measurementId: "G-SGKD8TXB95",
+// });
+
+// const initMessaging = firebase.messaging();
